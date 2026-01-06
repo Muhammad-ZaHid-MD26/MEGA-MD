e519ac7ebee5794933fa980ede4c316f93cbbe24
Muhammad-ZaHid-MD26 <muhammadzahid132181131314@gmail.com>
2026-01-06 21:36:53 +0500

    Update bot settings with new values

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
   prefixes: ['.', '!', '/', '#'], // Multiple prefix support you can add one or more
-  packname: 'MEGA-MD',
-  author: '‎GlobalTechInfo',
+  packname: '𝐌ꝛ 𝐃-ꭙ፝֯֟͜͡𝚗𝗍 𝐙𝐢𝐝𝐢𝐢',
+  author: '💳 •‎',
   timeZone: 'Asia/Karachi',
-  botName: "MEGA-MD", // your bot name
-  botOwner: 'Qasim Ali', // set your name
-  ownerNumber: '923051391007', // Set your number here without + symbol, just add country code & number without any space
+  botName: "𝐌ꝛ 𝐃-ꭙ፝֯֟͜͡𝚗𝗍 𝐙𝐢𝐝𝐢𝐢", // your bot name
+  botOwner: '𝐌ꝛ 𝐃-ꭙ፝֯֟͜͡𝚗𝗍 𝐙𝐢𝐝𝐢𝐢', // set your name
+  ownerNumber: '923288330026', // Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20,
@@ -14,8 +14,8 @@ const settings = {
   description: "This is a bot for managing group commands and automating tasks.",
   version: "5.1.0",
   updateZipUrl: "https://github.com/GlobalTechInfo/MEGA-MD/archive/refs/heads/main.zip",
-  channelLink: "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07",
-  ytch: "GlobalTechInfo"
+  channelLink: "https://chat.whatsapp.com/BjUoQ8MUwgfEB9DcfT3XX3",
+  ytch: "mr_d-xnt_zidii"
 };
 
 module.exports = settings;
